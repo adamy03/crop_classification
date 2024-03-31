e685d3008b2b573bce9f095078681a74791006e1
Adam Yang <66138604+adamy03@users.noreply.github.com>
2024-03-31 10:34:24 -0400

    clean up repo

--- dataset.py
+++ dataset.py
@@ -6,6 +6,10 @@ import torch
 from torch.utils.data import Dataset
 
 class CropDataset(Dataset):
+    """Standard dataset for pytorch dataloaders. 
+    Args:
+        Dataset (_type_): _description_
+    """
     def __init__(self, data_path, transform=None):
         self.path = data_path
         h5f = h5py.File(self.path, 'r')
@@ -25,6 +29,7 @@ class CropDataset(Dataset):
         sample, labels = torch.Tensor(sample.astype(float)).type(torch.float), torch.Tensor(labels.astype(int)).type(torch.int)
         
         if self.transform:
+            # Ensure labels undergo same transformation 
             fused = torch.cat((sample, labels))
             fused = self.transform(fused)
             sample, labels = fused[0:5], fused[5:8].reshape(3, fused.shape[1], fused.shape[2])
@@ -32,6 +37,8 @@ class CropDataset(Dataset):
         return sample, labels
     
 def one_hot_encode(labels):
+    """One hot encode data: convert classes in 2D to 3D with each channel representing positive for a corresponding class.
+    """
     out = np.zeros((3, labels.shape[1], labels.shape[2]), dtype=int)
     for i in range(labels.shape[1]):
         for j in range(labels.shape[2]):
@@ -39,6 +46,8 @@ def one_hot_encode(labels):
     return out
 
 def un_hot_encode(encoded):
+    """Convert one hot encoded outputs 2D class label image. Class value corresponds to channel.
+    """
     out = np.zeros((1, encoded.shape[1], encoded.shape[2]), dtype=int)
     for c in range(encoded.shape[0]):
         for i in range(encoded.shape[1]):
@@ -46,3 +55,18 @@ def un_hot_encode(encoded):
                 if encoded[c, i, j]:
                     out[0, i, j] = c 
     return out
+
+def divide_image(image, tile_size, stride):
+    """Splits images into tiles of specified size with given stride.
+    """
+    tiles = []
+    height, width, = image.shape[1], image.shape[2]
+    tile_height, tile_width = tile_size
+    stride_vertical, stride_horizontal = stride
+
+    for y in range(0, height - tile_height + 1, stride_vertical):
+        for x in range(0, width - tile_width + 1, stride_horizontal):
+            tile = image[::, y:y+tile_height, x:x+tile_width]
+            tiles.append(tile)
+
+    return tiles
